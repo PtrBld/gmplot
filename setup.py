a748143e479eab954901c1c9d75ebfba18aaec00
PtrBld <peter-bludau@gmx.de>
2018-03-20 10:33:39 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-__version__ = '1.2.0'
+__version__ = '1.2.1'
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
@@ -11,7 +11,7 @@ setup(
     version = __version__,
     author = 'Michael Woods',
     author_email = 'physicsmichael@gmail.com',
-    url = 'https://github.com/vgm64/gmplot',
+    url = 'https://github.com/PtrBld/gmplot',
     description = 'Provide a matplotlib like interface to plotting data with Google Maps',
     long_description=read('README.rst'),
     license='MIT',
